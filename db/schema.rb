534fdacb82111396a577ba13d81697bad70fb823
mib94 <mibnobachir@chicagobooth.edu>
2022-12-11 21:56:47 +0000

    user table created

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2022_12_11_215010) do
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
+    t.string "username"
+    t.integer "comments_count"
+    t.integer "likes_count"
+    t.boolean "private"
+    t.integer "sent_follow_requests_count"
+    t.integer "received_follow_requests_count"
+    t.integer "own_photos_count"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
