54cb0045d67833f78eabe7cb733128ba6740f2c6
mib94 <mibnobachir@chicagobooth.edu>
2022-12-12 01:11:21 +0000

    comments model complete

---
+++ app/models/comment.rb
@@ -0,0 +1,22 @@
+# == Schema Information
+#
+# Table name: comments
+#
+#  id         :integer          not null, primary key
+#  body       :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  author_id  :integer
+#  photo_id   :integer
+#
+class Comment < ApplicationRecord
+  belongs_to(:commenter, { :required => true, :class_name => "User", :foreign_key => "author_id" })
+
+  belongs_to(:photo, { :required => true, :class_name => "Photo", :foreign_key => "photo_id", :counter_cache => true })
+
+  validates(:photo_id, { :presence => true })
+
+  validates(:body, { :presence => true })
+
+  validates(:author_id, { :presence => true })
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_12_002959) do
+ActiveRecord::Schema.define(version: 2022_12_12_005236) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text "body"
+    t.integer "author_id"
+    t.integer "photo_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "follow_requests", force: :cascade do |t|
     t.integer "sender_id"
